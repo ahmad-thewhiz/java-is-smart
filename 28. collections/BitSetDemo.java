68c07e8a5be44e89559a4ea589d72b7267de3620
Ahmad Raza <inboxofahmad@gmail.com>
2025-01-19 23:27:56 +0530

    update

--- 28. collections/BitSetDemo.java
+++ 28. collections/BitSetDemo.java
@@ -29,13 +29,28 @@ public class BitSetDemo
        System.out.println("bs2: "+bs2);
         
         bs1.and(bs2);
-        System.out.println("bs1 AND bs2: "+bs1);
+        System.out.println("bs1 AND bs2 (new bs1): "+bs1);
 
         bs1.or(bs2);
-        System.out.println("bs1 OR bs2: "+bs1);
+        System.out.println("bs1 OR bs2 (new bs1): "+bs1);
+
+        // bs1.xor(bs2);
+        // System.out.println("bs1 XOR bs2 (new bs1): "+bs1);
+
+        // bs1.andNot(bs2);
+        // System.out.println("bs1 AND NOT bs2 (new bs1): "+bs1);
         
         bs1.flip(0,bs1.length());
         System.out.println("Flip: "+bs1);
+
+        System.out.println("Cardinality: "+bs1.cardinality());
+        System.out.println("Length: "+bs1.length());
+        System.out.println("Size: "+bs1.size());
+        System.out.println("Next Set Bit (from 0): "+bs1.nextSetBit(0));
+        // clear bit means set the bit to 0
+        System.out.println("Next Clear Bit (from 0): "+bs1.nextClearBit(0));
+        System.out.println("Previous Set Bit (from 9): "+bs1.previousSetBit(9));
+
         
     }  
 }
